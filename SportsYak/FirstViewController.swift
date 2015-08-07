8f2825c29fd74b2d6863a95653d9a3421a8ed099
Kurt <kjensen8292@Kurt-Jensens-MacBook-Pro.local>
2015-08-07 13:05:21 -0400

    Initial Commit

---
+++ SportsYak/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SportsYak
+//
+//  Created by Kurt Jensen on 8/7/15.
+//  Copyright (c) 2015 Arbor Apps. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SportsYak/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SportsYak
+//
+//  Created by Kurt Jensen on 8/7/15.
+//  Copyright (c) 2015 Arbor Apps. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
